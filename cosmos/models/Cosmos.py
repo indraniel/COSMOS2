4eb9b8a68326002a50d87598bebd096b26d74663
Indraniel Das <idas@genome.wustl.edu>
2016-09-05 18:22:15 -0500

    + adjust job name
      - square brackets (e.g. a '[' or ']') give a "bad job name" error in
        LSF 9
    
      - include the workflow name in the job name.
    
            This could allow for multiple workflow to occur simultaneously,
            given that each workflow has a unique name

--- cosmos/models/Cosmos.py
+++ cosmos/models/Cosmos.py
@@ -32,7 +32,7 @@ def default_get_submit_args(task, default_queue=None, parallel_env='orte'):
     mem_req = task.mem_req if use_mem_req else None
     time_req = task.time_req if use_time_req else None
 
-    jobname = '%s[%s]' % (task.stage.name, task.uid.replace('/', '_'))
+    jobname = '%s:%s(%s)' % (task.workflow.name, task.stage.name, task.uid.replace('/', '_'))
     default_queue = ' -q %s' % default_queue if default_queue else ''
     priority = ' -p %s' % default_job_priority if default_job_priority else ''
 
